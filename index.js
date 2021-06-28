0aefdc51e9441e61cec81c8bc9117bbba4a6e1b4
Rakib Hasan <mrakibhasan199744@gmail.com>
2021-06-29 00:28:58 +0600

    heroku problem solving

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m99d8.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
